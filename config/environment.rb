d68951b9aa16b81ddca6affc3ffeb3803a47de88
blaine <jbhatab@gmail.com>
2013-03-13 18:23:05 -0500

    s3 shit

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+
 # Initialize the rails application
 Mfn::Application.initialize!
 
-#heroku_san gem configuration
-#config.gem 'heroku_san'
+
 
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,12 @@ Mfn::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  #s3 and fog
+  config.action_controller.asset_host = Proc.new do |source, request|
+    scheme = request.ssl? ? "https" : "http"
+    "#{scheme}://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
+  end
+  
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
