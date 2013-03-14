d0693516c3a308ee387ee4d96d050894d911e7b3
blaine <jbhatab@gmail.com>
2013-03-13 19:03:11 -0500

    changed asset init

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -9,11 +9,11 @@ Mfn::Application.configure do
   config.action_controller.perform_caching = true
 
   #s3 and fog
-  config.action_controller.asset_host = Proc.new do |source, request|
+  config.action_controller.asset_host = Proc.new do |source, request = nil, *_|
     scheme = request.ssl? ? "https" : "http"
     "#{scheme}://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
   end
-  
+
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
