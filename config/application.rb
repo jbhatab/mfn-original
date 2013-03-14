b6fb51fd4e3a234fe13c7cdcf9e90be6465f4f46
blaine <jbhatab@gmail.com>
2013-03-14 01:50:15 -0500

    fixed fonts

--- config/application.rb
+++ config/application.rb
@@ -50,8 +50,8 @@ module Mfn
     config.filter_parameters += [:password]
 
     #font-face configuration
-    #config.assets.precompile += %w( .svg .eot .woff .ttf ) 
-    #config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    config.assets.precompile += %w( .svg .eot .woff .ttf ) 
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
     
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
