0b6fc0876630a7b82751987485151e1e6aa2f6e5
jbhatab <jbhatab@gmail.com>
2013-05-03 12:33:59 -0500

    refixed staging analytics

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -23,7 +23,7 @@ Mfn::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  GA.tracker = ""
+  GA.tracker = "UA-40489820-1"
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
