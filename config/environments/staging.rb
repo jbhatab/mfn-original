6b059be949033a635e5ca4641b3d604dae40556d
jbhatab <jbhatab@gmail.com>
2013-05-03 12:22:42 -0500

    analyitics in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -23,6 +23,8 @@ Mfn::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  GA.tracker = ""
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
