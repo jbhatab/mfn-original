692e92cde351df0d3adb7e3f41ee65c2a2697735
jbhatab <jbhatab@gmail.com>
2013-05-11 21:53:44 -0500

    fixed production config and event infowindow

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Mfn::Application.configure do
 
   GA.tracker = "UA-40489820-1"
 
+
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
