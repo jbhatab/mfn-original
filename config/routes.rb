95237886ecdf968ebb3098746ede8846ce439eb2
jbhatab <jbhatab@gmail.com>
2013-06-04 22:19:02 -0500

    footer, fixed rideshare stuff, and blogs started

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Mfn::Application.routes.draw do
 
 
+  resources :blogs
+
+
   match '/users/auth/:provider/callback' => 'authentications#create'
 
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, controllers: {omniauth_callbacks: "omniauth_callbacks"}
